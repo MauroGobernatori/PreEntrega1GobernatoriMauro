6267e9c8e1ce822e1a930a7c559142751eb793db
MauroGobernatori <mauro.gobernatori@gmail.com>
2023-03-23 19:55:12 -0300

    Second commit

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-let cantidad;
-let cantidad2;
-let diferente;
+let error_do = false;
+
+let cantidad = "";
+let cant_alumnos = 0;
+
+let sumatoria = 0;
+let nota = 0;
+
 do{
+    if(error_do){
+        alert("Ingrese un número entero");
+    }
     cantidad = prompt("Ingrese cantidad de alumnos");
-    cantidad2 = parseInt(cantidad);
-    diferente = cantidad != cantidad2;
-}while(diferente);
-
-// cantidad = prompt("Ingrese cantidad de alumnos");
-
-// console.log(cantidad);
-// console.log(typeof cantidad);
-
-// cantidad2 = parseInt(cantidad);
-
-// console.log(cantidad2);
-// console.log(typeof cantidad2);
+    cant_alumnos = parseInt(cantidad);
+    if(cantidad != cant_alumnos){
+        error_do = true;
+    }
+}while(cantidad != cant_alumnos);
 
 
-// console.log(cantidad != cantidad2);
+for(i=0; i<cant_alumnos; i++){
+    nota = prompt("Ingrese nota del alumno número " + (i+1) + ". (Solo números enteros)");
+    if(nota != parseInt(nota) || parseInt(nota) < 1 || parseInt(nota) > 10){
+        alert("La nota debe ser un número entero de 1 a 10");
+        i--;
+    }else{
+        sumatoria= sumatoria + parseInt(nota);
+    }
+}
+
+let promedio = sumatoria / cant_alumnos;
+
+console.log(cant_alumnos);
+console.log(sumatoria);
+console.log(promedio);
+
+alert("El promedio de las notas es: " + promedio);
