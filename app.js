1030aa926a693e6e386bdcbb1dd27806a7696563
MauroGobernatori <mauro.gobernatori@gmail.com>
2023-03-23 19:26:10 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let cantidad;
+let cantidad2;
+let diferente;
+do{
+    cantidad = prompt("Ingrese cantidad de alumnos");
+    cantidad2 = parseInt(cantidad);
+    diferente = cantidad != cantidad2;
+}while(diferente);
+
+// cantidad = prompt("Ingrese cantidad de alumnos");
+
+// console.log(cantidad);
+// console.log(typeof cantidad);
+
+// cantidad2 = parseInt(cantidad);
+
+// console.log(cantidad2);
+// console.log(typeof cantidad2);
+
+
+// console.log(cantidad != cantidad2);
